14f6aa62e823476397ae49ab48b93dd7e197408f
saicem <saicem@foxmail.com>
2021-07-15 23:37:24 +0800

    feat: 增加命令执行 swag init

--- initialize/router.go
+++ initialize/router.go
@@ -8,17 +8,29 @@ import (
 	"github.com/saicem/todo/router"
 	swaggerFiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
+	"os"
+	"os/exec"
 )
 
 func InitRouter() *gin.Engine {
 	engine := gin.New()
-
-	engine.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
-	fmt.Printf("open swagger UI http://localhost:%s/swagger/index.html\n", config.ProjectPort)
+	initSwagger(engine)
 	BasicGroup := engine.Group("api/v1", middleware.Cors)
 	router.RouteEmpty(BasicGroup)
 	router.RouteTodo(BasicGroup)
 	router.RouteUser(BasicGroup)
 
 	return engine
+}
+
+func initSwagger(engine *gin.Engine) {
+	cmd := exec.Command("swag", "init")
+	cmd.Stdout = os.Stdout
+	err := cmd.Run()
+	if err != nil {
+		// todo 更高的错误等级
+		panic(err)
+	}
+	engine.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+	fmt.Printf("open swagger UI http://localhost:%s/swagger/index.html\n", config.ProjectPort)
 }
