9c2fe151e0cfa6712bcfee1d5ea543be2161b448
saicem <saicem@foxmail.com>
2021-07-15 17:54:52 +0800

    fix: 修复因为 omitempty 导致不返回 status = 0 的问题

--- model/response/common.go
+++ model/response/common.go
@@ -1,7 +1,7 @@
 package response
 
 type Response struct {
-	Status  ApiCode     `json:"status,omitempty"`
+	Status  ApiCode     `json:"status"`
 	Message string      `json:"message,omitempty"`
 	Data    interface{} `json:"data,omitempty"`
 }
